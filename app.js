e6079cfb3c3f4e7a56033895a87396ab34efe7af
Ikeda <brandon_ikeda@hotmail.com>
2017-10-25 13:34:17 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+
+var app = express();
+
+var routes = require('./routes');
+
+app.set('view engine', 'ejs');
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+
+// Home
+app.get('/', routes.home);
+
+// Movie Single
+app.get('/starwars_episode/:episodenumber?', routes.movie_single);
+
+// Not Found - 404
+app.get('*', routes.notFound);
+
+app.listen(3000, function(){
+  console.log("The application is running on localhost:3000");
+});
---
+++ routes/index.js
@@ -0,0 +1,45 @@
+// Get Movies.json
+var moviesJSON = require('../movies.json');
+
+//Routes
+
+// Home
+exports.home = function(req,res){
+  var movies = moviesJSON.movies;
+  res.render('home', {
+    title: "Star Wars Movies",
+    movies: movies
+  });
+};
+
+// Movie Single
+exports.movie_single = function(req,res){
+  var episodenumber = req.params.episodenumber;
+  var movies = moviesJSON.movies;
+
+  if(episodenumber >= 1 && episodenumber <= 6){
+    var movie = movies[episodenumber - 1];
+    var title = movie.title;
+    var main_characters = movie.main_characters;
+    res.render('movie_single', {
+      movies: movies,
+      title: title,
+      movie: movie,
+      main_characters: main_characters
+    });
+}else{
+    res.render('notFound', {
+      movies: movies,
+      title: "This is not the page you are looking for - Obi Wan"
+    });
+}
+};
+
+// Not Found - 404
+exports.notFound = function(req,res){
+  var movies = moviesJSON.movies;
+  res.render('notFound', {
+    movies: movies,
+    title: "This is not the page you are looking for - Obi Wan"
+  });
+};
